1ffd16e1e3e37beb2eaec5bedff32fc7797cc7e3
Cauim de Souza Lima <cauimsouza@gmail.com>
2023-04-11 22:30:14 +0000

    Implement scanner.

--- clox/main.c
+++ clox/main.c
@@ -1,33 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "value.h"
 #include "debug.h"
 #include "vm.h"
 
+static void repl() {
+    char line[1024];
+    
+    for (;;) {
+        printf("> ");
+        
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        
+        interpret(line);
+    }
+}
+
+static char *readFile(const char *path) {
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+    
+    fseek(file, 0L, SEEK_END);
+    size_t file_size = ftell(file);
+    rewind(file);
+    
+    char *buffer = (char *) malloc(file_size + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read file \"%s\".\n", path);
+        exit(74);
+    }
+    
+    size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+    if (bytes_read < file_size) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+    
+    buffer[bytes_read] = '\0';
+    
+    fclose(file);
+    
+    return buffer;
+}
+
+static void runFile(const char *path) {
+    char *source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+    
+    if (result == INTERPRET_COMPILER_ERROR) {
+        exit(65);
+    }
+    if (result == INTERPRET_RUNTIME_ERROR) {
+        exit(70);
+    }
+}
+
 int main(int argc, const char *argv[]) {
-    Chunk chunk;
-    initChunk(&chunk);
-    
-    writeConstant(&chunk, 1, 123);
-    writeConstant(&chunk, 2, 123);
-    writeConstant(&chunk, 3, 123);
-    writeChunk(&chunk, OP_MULTIPLY, 123);
-    writeChunk(&chunk, OP_ADD, 123);
-    
-    writeConstant(&chunk, 4, 123);
-    writeConstant(&chunk, 5, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
-    writeChunk(&chunk, OP_DIVIDE, 123);
-    writeChunk(&chunk, OP_SUBTRACT, 123);
-    
-    writeChunk(&chunk, OP_RETURN, 123);
-    
-    //disassembleChunk(&chunk, "test chunk");
     initVM();
-    interpret(&chunk);
-    freeVM();
     
-    freeChunk(&chunk);
+    if (argc == 1) {
+        repl();
+    } else if (argc == 2) {
+        runFile(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: clox [path]\n");
+    }
+    
+    freeVM();
     
     return 0;
 }
---
+++ clox/scanner.c
@@ -0,0 +1,279 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "common.h"
+#include "scanner.h"
+
+typedef struct {
+  const char *start;
+  const char *current;
+  int line;
+} Scanner;
+
+Scanner scanner;
+
+void initScanner(const char *source) {
+    scanner.start = source;
+    scanner.current = source;
+    scanner.line = 1;
+}
+
+static char current() {
+    return *scanner.current;
+}
+
+static char next() {
+    return *(scanner.current + 1);
+}
+
+static char advance() {
+    return *scanner.current++;
+}
+
+static bool match(char c) {
+    if (*scanner.current == c) {
+        scanner.current++;
+        return true;
+    }
+    return false;
+}
+
+static bool isAtEnd() {
+    return current() == '\0';
+}
+
+static bool isAlpha(char c) {
+    return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_';
+}
+
+static bool isAlphaNum(char c) {
+    return isAlpha(c) || isdigit(c);
+}
+
+static Token makeToken(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int) (scanner.current - scanner.start);
+    token.line = scanner.line;
+    
+    return token;
+}
+
+static Token errorToken(const char *message) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = (int) strlen(message);
+    token.line = scanner.line;
+    
+    return token;
+}
+
+static Token scanIdentifierOrKeyword() {
+    while (isAlphaNum(current())) {
+        scanner.current++;
+    }
+    
+    char lexeme[64];
+    char *lexemep = lexeme;
+    for (const char *c = scanner.start; c != scanner.current; c++) {
+        *lexemep = *c;
+        lexemep++;
+    }
+    *lexemep = '\0';
+    
+    TokenType type = TOKEN_IDENTIFIER;
+    if (!strcmp(lexeme, "and")) {
+        type = TOKEN_AND;
+    } else if (!strcmp(lexeme, "class")) {
+        type = TOKEN_CLASS;
+    } else if (!strcmp(lexeme, "else")) {
+        type = TOKEN_ELSE;
+    } else if (!strcmp(lexeme, "false")) {
+        type = TOKEN_FALSE;
+    } else if (!strcmp(lexeme, "for")) {
+        type = TOKEN_FOR;
+    } else if (!strcmp(lexeme, "fun")) {
+        type = TOKEN_FUN;
+    } else if (!strcmp(lexeme, "if")) {
+        type = TOKEN_IF;
+    } else if (!strcmp(lexeme, "nil")) {
+        type = TOKEN_NIL;
+    } else if (!strcmp(lexeme, "or")) {
+        type = TOKEN_OR;
+    } else if (!strcmp(lexeme, "print")) {
+        type = TOKEN_PRINT;
+    } else if (!strcmp(lexeme, "return")) {
+        type = TOKEN_RETURN;
+    } else if (!strcmp(lexeme, "super")) {
+        type = TOKEN_SUPER;
+    } else if (!strcmp(lexeme, "this")) {
+        type = TOKEN_THIS;
+    } else if (!strcmp(lexeme, "true")) {
+        type = TOKEN_TRUE;
+    } else if (!strcmp(lexeme, "var")) {
+        type = TOKEN_VAR;
+    } else if (!strcmp(lexeme, "while")) {
+        type = TOKEN_WHILE;
+    }
+    
+    return makeToken(type);
+}
+
+static Token scanNumber() {
+    while (isdigit(current())) {
+        advance();
+    }
+    
+    if (!match('.')) {
+        return makeToken(TOKEN_NUMBER);
+    }
+    
+    while (isdigit(current())) {
+        advance();
+    }
+    
+    return makeToken(TOKEN_NUMBER);
+}
+
+static Token scanString() {
+    for (;;) {
+        if (isAtEnd()) {
+            break;
+        }
+        
+        if (match('"')) {
+            return makeToken(TOKEN_STRING);
+        }
+        
+        if (match('\\') && isAtEnd()) {
+            break;
+        }
+        
+        advance();
+    }
+    
+    return errorToken("Non-terminated string literal.");
+}
+
+static void skipSingleLineComment() {
+    for (;;) {
+        if (isAtEnd()) {
+            return;
+        }
+        if (match('\n')) {
+            scanner.line++;
+            return;
+        }
+        advance();
+    }
+}
+
+static bool skipMultiLineComment() {
+    for (;;) {
+        if (isAtEnd()) {
+            return false;
+        }
+        
+        if (current() == '*' && next() == '/') {
+            advance();
+            advance();
+            return true;
+        } 
+        advance();
+    }
+}
+
+static void skipSpace(char c) {
+    for (;;) {
+        if (c == '\n') {
+            scanner.line++;
+        }
+        if (isspace(current())) {
+            c = advance();
+            continue;
+        }
+        return;
+    }
+}
+
+Token scanToken() {
+    scanner.start = scanner.current;
+    
+    if (isAtEnd()) {
+        return makeToken(TOKEN_EOF);
+    }
+
+    char c = advance();
+    switch (c) {
+        case '(':
+            return makeToken(TOKEN_LEFT_PAREN);
+        case ')':
+            return makeToken(TOKEN_RIGHT_PAREN);
+        case '{':
+            return makeToken(TOKEN_LEFT_BRACE);
+        case '}':
+            return makeToken(TOKEN_RIGHT_BRACE);
+        case ';':
+            return makeToken(TOKEN_SEMICOLON);
+        case ',':
+            return makeToken(TOKEN_COMMA);
+        case '.':
+            return makeToken(TOKEN_DOT);
+        case '-':
+            return makeToken(TOKEN_MINUS);
+        case '+':
+            return makeToken(TOKEN_PLUS);
+        case '/':
+            if (match('/')) {
+                skipSingleLineComment();
+                return scanToken();
+            }
+            if (match('*')) {
+                if (skipMultiLineComment()) {
+                    return scanToken();
+                }
+                return errorToken("Non-terminated multi-line comment.");
+            }
+            return makeToken(TOKEN_SLASH);
+        case '*':
+            return makeToken(TOKEN_STAR);
+        case '!':
+            if (match('=')) {
+                return makeToken(TOKEN_BANG_EQUAL);
+            }
+            return makeToken(TOKEN_BANG);
+        case '=':
+            if (match('=')) {
+                return makeToken(TOKEN_EQUAL_EQUAL);
+            }
+            return makeToken(TOKEN_EQUAL);
+        case '>':
+            if (match('=')) {
+                return makeToken(TOKEN_GREATER_EQUAL);
+            }
+            return makeToken(TOKEN_GREATER);
+        case '<':
+            if (match('=')) {
+                return makeToken(TOKEN_LESS_EQUAL);
+            }
+            return makeToken(TOKEN_LESS);
+        case '"':
+            return scanString();
+        default:
+            if (isspace(c)) {
+                skipSpace(c);
+                return scanToken();
+            }
+            if (isAlpha(c)) {
+                return scanIdentifierOrKeyword();
+            }
+            if (isdigit(c)) {
+                return scanNumber();
+            }
+    }
+    
+    return errorToken("Unexpected character.");
+}
